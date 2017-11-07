b8ac31abb153f408690970e00a9a2f5c643585dc
David Butler <dbutler4@hawk.iit.edu>
2017-11-07 18:07:10 +0000

    Added cancel function to stop alarm

--- temperature.py
+++ temperature.py
@@ -3,17 +3,20 @@ from timer import run_timer
 from timer import clear
 import time
 
+cancelled = False
 def temperature():
 	sense = SenseHat()
 	running = True
 	baseTemperature = sense.get_temperature() #temperature of pi when initially started
-	while running:
+	while running and not cancelled:
 		time.sleep(.5)
 		currTemperature = sense.get_temperature() #updated temperature
-		if(abs(baseTemperature-currTemperature) >= .5):#checks if temperature has changed by .5 degrees since system started
+		if(abs(baseTemperature-currTemperature) >= .5): #checks if temperature has changed by .5 degrees since system started
 			securityProtocol(baseTemperature - currTemperature)
 			break
 
+def cancel():
+	cancelled = True
 def securityProtocol(temperatureDifference):
 	#text("Temperature change of: " + temperatureDifference + "detected!\nText this number your password if this was you.")
 	run_timer()
