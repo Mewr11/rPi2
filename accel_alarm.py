7d1c08461fbd2a710602318d43743c25afc97fbf
Franklin Navarro <rubicksolomid@gmail.com>
2017-11-09 18:01:12 +0000

    added actual accel alarm

---
+++ accel_alarm.py
@@ -0,0 +1,27 @@
+from sense_hat import SenseHat
+from time import sleep
+from timer import 
+
+class accel_alarm():
+	def __init__(self):
+		self.sensor = SenseHat()
+		self.running = False
+		self.trigger_force = 2
+	def arm(self):
+		self.running = True
+		self.loop()
+	def disarm(self):
+		self.running = False
+	def loop(self):
+		while(self.running):
+			data = self.sensor.get_accelerometer_raw()
+			print(data)
+			if(data["x"] > self.trigger_force or data["y"] > self.trigger_force or data["z"] > self.trigger_force):
+				print("SOUND THE ALARM")
+				self.disarm()
+				run_timer()
+			sleep(1)
+
+if(__name__ == "__main__"):
+	alarm = accel_alarm()
+	alarm.arm()
